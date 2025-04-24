d813f14e7f94fb9cc6629aa8de8752070e5b9fcb
Yoshida0110 <joshuaemills0110@gmail.com>
2025-04-24 21:06:15 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Cairo server is live');
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/messages", (req, res) => {
+  res.json([]);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Cairo server is live on port ${PORT}`);
+});
