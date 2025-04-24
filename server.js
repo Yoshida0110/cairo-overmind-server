308320a6584ce97ef10499d1534ff937d48d3a92
Yoshida0110 <joshuaemills0110@gmail.com>
2025-04-24 21:26:44 +1000

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,24 @@ const app = express();
 
 app.use(express.json());
 
+let messages = [];
+
 app.get("/api/messages", (req, res) => {
-  res.json([]);
+  res.json(messages);
+});
+
+app.post("/api/messages", (req, res) => {
+  const message = req.body.message;
+  if (message) {
+    messages.push({ message, time: new Date().toISOString() });
+    res.status(201).json({ success: true });
+  } else {
+    res.status(400).json({ success: false, error: "No message provided" });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Cairo server is live on port ${PORT}`);
 });
+
